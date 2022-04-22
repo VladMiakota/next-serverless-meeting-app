56b2428ed8f8cbe01162b25aaeee97eae881e97c
Vladyslav Miakota <vladyslav.miakota@aimprosoft.com>
2022-04-22 19:16:11 +0300

    change AWS link

--- client/api/axiosConfig.ts
+++ client/api/axiosConfig.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const baseURL = process.env.BASE_URL || 'http://127.0.0.1:3000'
+const AWS_URL = 'https://y5vm7o1v7a.execute-api.us-east-1.amazonaws.com/Prod/'
+const baseURL = AWS_URL || 'http://127.0.0.1:3000'
 
 const axiosConfig = axios.create({
     baseURL: baseURL,
